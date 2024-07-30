a6d38b1b4e26c60c052f7044c1e89b01902cdde4
ABDULLAH-LAPTOP\mdn522 <mdn522@gmail.com>
2024-07-30 12:28:30 +0600

    Added marked for review in charts

--- charts/views.py
+++ charts/views.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
@@ -37,14 +38,23 @@ def chart_view(request, user_id=None, username=None):
 
     # dynamic past 15 days
     DAYS = 30
+    try:
+        DAYS = int(request.GET.get('days'))
+    except:
+        pass
+    DAYS = min(DAYS, 90)
+
+    date__gte = (timezone.now() - timezone.timedelta(days=DAYS)).date()
+
+    # Attempts Chart
     answer_set = user \
         .question_answer_set \
-        .filter(answered_at__gte=(timezone.now() - timezone.timedelta(days=DAYS)).date()) \
+        .filter(answered_at__gte=date__gte) \
         .prefetch_related('question') \
         .only(*['answered_at', 'started_at', 'time_given', 'is_correct', 'question__module'])
 
     # print(get_date_range(timezone.now() - timezone.timedelta(days=DAYS), timezone.now()))
-    x_axis = [dt.strftime('%Y-%m-%d') for dt in get_date_range(timezone.now() - timezone.timedelta(days=30 - 1), timezone.now())]
+    x_axis = [dt.strftime('%Y-%m-%d') for dt in get_date_range(timezone.now() - timezone.timedelta(days=DAYS - 1), timezone.now())]
     answer_data = {
         'x_axis': x_axis,
         Question.Module.MATH.value: {
@@ -58,11 +68,42 @@ def chart_view(request, user_id=None, username=None):
     }
     for answer in answer_set:
         answer_data[answer.question.module][answer.is_correct][x_axis.index(answer.answered_at.strftime('%Y-%m-%d'))] += 1
-        pass
+
+    status_set = user \
+        .question_status_set \
+        .filter(Q(marked_for_review_at__gte=date__gte) | Q(unmarked_for_review_at__gte=date__gte)) \
+        .prefetch_related('question') \
+        .only(*['is_marked_for_review', 'marked_for_review_at', 'unmarked_for_review_at', 'question__module'])
+
+    mark_data = {
+        'x_axis': x_axis,
+        Question.Module.MATH.value: {
+            0: [0] * DAYS,
+            1: [0] * DAYS,
+        },
+        Question.Module.ENGLISH.value: {
+            0: [0] * DAYS,
+            1: [0] * DAYS,
+        },
+    }
+
+    for status in status_set:
+        dt = ""
+        if status.is_marked_for_review:
+            dt = status.marked_for_review_at
+        else:
+            dt = status.unmarked_for_review_at
+        if dt:
+            dts = dt.strftime('%Y-%m-%d')
+            x_index = x_axis.index(dts)
+            if mark_data[status.question.module][status.is_marked_for_review][x_index] is None:
+                mark_data[status.question.module][status.is_marked_for_review][x_index] = 0
+            mark_data[status.question.module][status.is_marked_for_review][x_index] += 1
 
     # print('answer_data', answer_data)
 
     return render(request, 'basic/pages/charts/index.html', context={
+        'current_user': user,
         'answer_data': answer_data,
-        'current_user': user
+        'mark_data': mark_data,
     })
