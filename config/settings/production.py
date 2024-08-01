5a8b3f67fe7c78af5474f2dde410639eabeb75f2
ABDULLAH-LAPTOP\mdn522 <mdn522@gmail.com>
2024-08-01 10:45:58 +0600

    logger check

--- config/settings/production.py
+++ config/settings/production.py
@@ -119,7 +119,7 @@ ANYMAIL = {}
 
 LOGGING = {
     "version": 1,
-    "disable_existing_loggers": True,
+    "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s",
