ed633fe719ca7c9c91a826e12894659ed2e5d99f
ABDULLAH-LAPTOP\mdn522 <mdn522@gmail.com>
2024-08-06 14:45:06 +0600

    Added arbitrary base path support in question loader

--- utils/scraper/loaders/__init__.py
+++ utils/scraper/loaders/__init__.py
@@ -1,6 +1,8 @@
 # from . import collegeboard, princetonreview, satmocks
 from typing import List
 
+from django.conf import settings
+
 from programs.models import Program
 
 from pathlib import Path
@@ -13,14 +15,14 @@ class Loader:
         return program
 
     @staticmethod
-    def get_file_list(path, glob, base_path: Path = None) -> List[Path]:
+    def get_file_list(path, glob, base_path: Path = settings.Q_LOADER_BASE_PATH) -> List[Path]:
         data_path = (base_path / path) if base_path else Path('') / 'data' / 'sat' / path
 
         files = list(data_path.glob(glob))
         return files
 
     @staticmethod
-    def get_file(path, file, base_path=None) -> Path:
+    def get_file(path, file, base_path = settings.Q_LOADER_BASE_PATH) -> Path:
         data_path = base_path if base_path else Path('') / 'data' / 'sat'
         return data_path / path / file
 
