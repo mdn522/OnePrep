b9935383573725abe9d9bc82111bd9d68fbe515a
ABDULLAH-LAPTOP\mdn522 <mdn522@gmail.com>
2024-08-28 17:41:38 +0600

    removed ability to see user's chart with user_id to avoid leaking user data

--- charts/urls.py
+++ charts/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = 'charts'
 
 urlpatterns = [
-    path("user/<int:user_id>/charts/", views.chart_view, name="chart_with_user_id"),
+    # path("user/<int:user_id>/charts/", views.chart_view, name="chart_with_user_id"),
     path("user/<str:username>/charts/", views.chart_view, name="chart_with_username"),
     path("user/charts/", views.chart_view, name="chart"),
 
 
-    path("charts/user/<int:user_id>/exam/<int:exam_id>/basic-time", views.basic_exam_time_view),
+    # path("charts/user/<int:user_id>/exam/<int:exam_id>/basic-time", views.basic_exam_time_view),
     path("charts/user/<str:username>/exam/<int:exam_id>/basic-time", views.basic_exam_time_view),
     path("charts/user/exam/<int:exam_id>/basic-time", views.basic_exam_time_view, name="basic_exam_time"),
 ]
