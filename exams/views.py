d6f854b14e3b7509e5873c22055d857a3ab3dc6a
mdn522 <mdn522@gmail.com>
2024-09-23 09:52:53 +0600

    added filtering in exams list

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,4 @@
+import django_filters
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count, Min, F, OuterRef, Subquery, Q, PositiveIntegerField
@@ -8,6 +9,14 @@ from core.models import SubqueryCount
 from questions.models import Question, UserQuestionAnswer, UserQuestionStatus
 from .models import Exam
 
+
+class ExamFilter(django_filters.FilterSet):
+    class Meta:
+        model = Exam
+        fields = ['source', 'module', 'official']
+
+
+cache = {}
 
 class ExamListView(ListView):
     model = Exam
@@ -33,7 +42,6 @@ class ExamListView(ListView):
                 .values('count')
         )
 
-
         question_answer = (
             UserQuestionAnswer.objects
                 .filter(question__exam_question_set__exam=OuterRef('pk'), user=user, exam=None, is_correct=True).distinct('question')
@@ -55,6 +63,27 @@ class ExamListView(ListView):
                 .order_by('source', 'source_order', 'name')
         )
 
+
         return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        qs = self.get_queryset()
+        filter = ExamFilter(self.request.GET, qs)
+        ctx["filter"] = filter
+
+        if 'sources' not in cache:
+            sources = Exam.objects.filter(is_active=True).values_list('source', flat=True).order_by('source').distinct()  # TODO cache
+            cache['sources'] = sources  # TODO Use better caching with timeout
+
+        ctx['sources'] = cache['sources']
+        ctx['sources_friendly_names'] = {
+            'collegeboard_bluebook': 'College Board Bluebook',
+            'sat_panda': 'SAT Panda',
+            'princeton_review': 'The Princeton Review',
+            'satmocks': 'SAT® Mocks',
+            'test_ninjas': 'Test Ninjas',
+        }
+        return ctx
 
 
